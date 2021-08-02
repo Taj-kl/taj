afe28cf3731535aa4cc3b37a67b6e6a4cc9f6f24
Taj-kl <88119894+Taj-kl@users.noreply.github.com>
2021-08-02 11:29:32 +0930

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
+    motors.largeAD.tank(100, 100, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(90, 90, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(80, 80, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(70, 70, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(60, 60, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(50, 50, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(40, 40, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(30, 30, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(20, 20, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(10, 10, 1, MoveUnit.Rotations)
+    motors.largeAD.tank(1000, 1000, 4, MoveUnit.Rotations)
+    motors.largeAD.tank(-100, 100, 1, MoveUnit.Rotations)
+})
+forever(function () {
+	
+})
